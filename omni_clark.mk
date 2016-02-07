d6fffaafd84d1828423806e1e521b638b21de0f3
HashBang <hashbang173@gmail.com>
2016-02-07 16:37:43 -0500

    omni-fy

--- omni_clark.mk
+++ omni_clark.mk
@@ -2,11 +2,8 @@
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 2560
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/clark/aosp_clark.mk)
@@ -14,7 +11,7 @@ $(call inherit-product, device/moto/clark/aosp_clark.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_clark
+PRODUCT_NAME := omni_clark
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := XT1575
 
