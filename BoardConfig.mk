3d4ee8a8dcbef71a088617a7943cc227695a1bee
Hashbang173 <hashbang173@gmail.com>
2018-12-21 11:16:43 -0500

    clark: move blobs to tree
    Change-Id: I36e4b91977c37866ecd732f48524f135971ac22c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,8 +137,11 @@ TARGET_RIL_VARIANT := caf
 include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/legacy-sepolicy.mk
 
-# TWRP Support - Optional
-ifeq ($(WITH_TWRP),true)
--include device/moto/clark/twrp.mk
-endif
+RECOVERY_VARIANT := twrp
 
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+TW_NEW_ION_HEAP := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_QCOM_RTC_FIX := true
