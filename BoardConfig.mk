cad262f3a6c0224a257f90369c415c73ed3f56c9
Hashbang173 <hashbang173@gmail.com>
2018-12-21 11:06:33 -0500

    clark: use prebuilt kernel
    Change-Id: If37581305e3ba11a444a1f8c489d23d4eb78f7c8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,15 +113,13 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=none androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 earlyprintk=msm_hsl_uart,0xf991e000 utags.blkdev=/dev/block/platform/soc.0/f9824900.sdhci/by-name/utags utags.backup=/dev/block/platform/soc.0/f9824900.sdhci/by-name/utagsBackup boot_cpus=0-5 androidboot.selinux=permissive
-BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE :=  4096
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CONFIG := clark_defconfig
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8992
+BOARD_KERNEL_IMAGE_NAME := Image
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
